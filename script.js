b04edb19022b741186bb32429efbbebe2c56e99c
emma673r <90177139+emma673r@users.noreply.github.com>
2022-10-15 17:13:47 +0200

    rest of animations done

--- script.js
+++ script.js
@@ -20,9 +20,8 @@ const rocketKF = [
 ];
 
 const shuffleProp = {
-  duration: 500,
+  duration: 300,
   easing: "linear",
-  animationDelay: 2000,
 };
 
 const shuffleKF = [
@@ -35,51 +34,123 @@ const shuffleKF = [
   { offset: 1, transform: "translateX(0px)" },
 ];
 
+const rotateProp = {
+  duration: 5000,
+  easing: "ease-in-out",
+};
+
+const rotateKF = [
+  { offset: 0, transform: "rotate(0deg)" },
+  {
+    offset: 1,
+    transform: "rotate(180deg)",
+    fill: "var(--color-accent1)",
+  },
+];
+
+const popProp = {
+  duration: 300,
+  easing: "ease-in-out",
+};
+
+const popKF = [
+  {
+    offset: 0.4,
+    transform: "scale(1.2)",
+  },
+  {
+    offset: 1,
+    transform: "scale(0)",
+  },
+];
+
+const pulseProp = {
+  duration: 1000,
+  easing: "ease-in-out",
+};
+
+const pulseKF = [
+  { offset: 0, transform: "scale(1)" },
+
+  {
+    offset: 0.25,
+    transform: "scale(0.8)",
+  },
+  {
+    offset: 0.5,
+    transform: "scale(1)",
+  },
+  {
+    offset: 0.75,
+    transform: "scale(0.8)",
+  },
+  {
+    offset: 1,
+    transform: "scale(1)",
+    fill: "var(--color-accent1)",
+  },
+];
+
 function start() {
+  // rocket launches when btn clicked
   document.getElementById("rocket-btn").addEventListener("click", () => {
     const rocketAnim = document.getElementById("rocket").animate(rocketKF, rocketProp);
   });
 
+  document.querySelector("#learn-btn").addEventListener("click", () => {
+    document.querySelector("#learn-btn").animate(popKF, popProp);
+  });
+
+  // TODO cogs do smthng when btn clicked
+  document.querySelector("#cog-btn").addEventListener("click", () => {
+    const rotateAnim = document.querySelector("#cog").animate(pulseKF, pulseProp);
+  });
+
+  // TODO workd cogs turn when btn clicked
+
+  document.querySelector("#demo").addEventListener("click", () => {
+    const rotateAnim = document.querySelector("#world-cog").animate(rotateKF, rocketProp);
+  });
+
+  //  demo button shuffles - random timing and on hover
   const demoBtns = document.querySelectorAll(".demoBtn");
-
-
   demoBtns.forEach((btn) => {
     btn.addEventListener("mouseover", (thisbtn) => {
-    //   console.log(thisbtn.target);
+      //   console.log(thisbtn.target);
       let thisBtn = thisbtn.target;
 
       thisBtn.animate(shuffleKF, shuffleProp);
     });
   });
 
-    setTimeout(() => {
+  setTimeout(() => {
     //   console.log("it called the function");
+    animateShuffle();
+    setTimeout(() => {
+      // console.log("it called the function again");
       animateShuffle();
       setTimeout(() => {
-        // console.log("it called the function again");
+        //   console.log("it called the function again and again");
         animateShuffle();
         setTimeout(() => {
-        //   console.log("it called the function again and again");
+          // console.log("it called the function 4times");
           animateShuffle();
           setTimeout(() => {
-            // console.log("it called the function 4times");
+            //   console.log("it called the function 5");
             animateShuffle();
             setTimeout(() => {
-            //   console.log("it called the function 5");
+              // console.log("it called the function 6");
               animateShuffle();
               setTimeout(() => {
-                // console.log("it called the function 6");
-                animateShuffle();
-                setTimeout(() => {
                 //   console.log("it called the function 7");
-                  animateShuffle();
-                }, Math.floor(Math.random() * 9000));
-              }, Math.floor(Math.random() * 12000));
-            }, Math.floor(Math.random() * 7000));
-          }, Math.floor(Math.random() * 20000));
-        }, Math.floor(Math.random() * 12000));
-      }, Math.floor(Math.random() * 8000));
-    }, Math.floor(Math.random() * 11000));
+                animateShuffle();
+              }, Math.floor(Math.random() * 9000));
+            }, Math.floor(Math.random() * 12000));
+          }, Math.floor(Math.random() * 7000));
+        }, Math.floor(Math.random() * 20000));
+      }, Math.floor(Math.random() * 12000));
+    }, Math.floor(Math.random() * 8000));
+  }, Math.floor(Math.random() * 11000));
 
   function animateShuffle(btn) {
     document.querySelectorAll(".demoBtn").forEach((btn) => {
